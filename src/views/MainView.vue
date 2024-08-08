ef4a590177cee609272fa52f77826e0fb0a0d238
Shloimy <shloimyelbaum@gmail.com>
2024-08-08 17:19:46 +0300

    with nursery

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,5 +1,13 @@
 <template>
-    <main class="bg-cyan-200 w-full h-screen"></main>
+  <main class="bg-transparent w-full h-screen flex justify-center items-center">
+    <div class="absolute inset-0 z-[-1]">
+      <img
+        src="/images/nursery-with-plaque.png"
+        alt="Background"
+        class="w-full h-full object-cover"
+      />
+    </div>
+  </main>
 </template>
 
 <script setup></script>
