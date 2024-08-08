af6c470f56f2c9bb8934ca156b24b69dcc216eae
Shloimy <shloimyelbaum@gmail.com>
2024-08-08 14:53:47 +0300

    intialized project again with vite

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  // Port for the server
+  server: {
+    port: 4000
+  }
+})
